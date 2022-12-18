3ddaa498a466a1d19f0aa7a3af3b3d2300164a29
Kevin Hung <kevincjhung@gmail.com>
2022-12-18 15:49:12 -0800

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,2 +1,13 @@
-import { app } from '../app'
+import app  from '../app'
+import request from 'supertest'
+
+// create a test to test the http api
+describe('GET /api/moods', () => {
+    test('should return 200 OK', () => {
+        return request(app).get('/api/moods').expect(200)
+    })
+    }
+)
+
+
 
