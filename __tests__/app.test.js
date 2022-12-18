a1bf4a72402a973f6ac4fee9347b8fca0236e086
Kevin Hung <kevincjhung@gmail.com>
2022-12-17 17:33:35 -0800

    .

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,18 +1,2 @@
-import request from 'supertest'
-import express from 'express'
+import { app } from '../app'
 
-
-import app from '../app'
-
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
-});
-
-request(app, { http2: true })
-  .get('/user')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '15')
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
-  });
--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('../sum');
+import sum from '../sum'
 
 describe('sum', () => {
   test('adds 1 + 2 to equal 3', () => {
